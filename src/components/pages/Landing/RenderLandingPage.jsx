63ebfc26eae6308cb88d55a7641abf9c0ac59118
Fatimah Ismael <fatimahismael@yahoo.com>
2023-06-11 14:21:02 -0700

    bottom section and css updated

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -33,13 +33,14 @@ function RenderLandingPage(props) {
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
       {/* <div className="graphs-section"> */}
-      <section className="graph-section">
+      <section className="graphs-section">
         <div className="graph-charts">
           <img
             className="graph-img"
             src={GrantRatesByOfficeImg}
             alt="bar-graph"
           ></img>
+          <h2>Search Grant Rates By Office</h2>
         </div>
         <div className="graph-charts">
           <img
@@ -47,6 +48,7 @@ function RenderLandingPage(props) {
             src={GrantRatesByNationalityImg}
             alt="pie-chart"
           ></img>
+          <h2>Search Grant Rates By Nationality</h2>
         </div>
         <div className="graph-charts">
           <img
@@ -54,6 +56,7 @@ function RenderLandingPage(props) {
             src={GrantRatesOverTimeImg}
             alt="line -graph"
           ></img>
+          <h2>Search Grant Rates Over Time</h2>
         </div>
       </section>
 
@@ -66,8 +69,17 @@ function RenderLandingPage(props) {
           View the Data
         </Button>
       </div>
+      <div className="download-the-data-btn-container">
+        <Button
+          type="default"
+          style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+          onClick={() => history.push('/graphs')}
+        >
+          Download the Data
+        </Button>
+      </div>
 
-      <div className="middle-section">
+      <section className="middle-section">
         <div className="hrf-img-container">
           <img src={HrfPhoto} alt="Human Rights First" className="hrf-img" />
         </div>
@@ -82,14 +94,43 @@ function RenderLandingPage(props) {
             the data set
           </h3>
         </div>
-      </div>
-      <div>
-        {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
-        <p onClick={() => scrollToTop()} className="back-to-top">
-          Back To Top ^
-        </p>
-      </div>
+      </section>
+
+      {/* Bottom Section: Add code here for the graphs section for your first ticket */}
+      {/* <div className="bottom-section">*/}
+      <section className="bottom-section">
+        <h1>Systemic Disparity Insights</h1>
+        <div className="data-container">
+          <div className="bottom-section-text-container">
+            <h1>36%</h1>
+            <p>
+              By the end of the Trump administration, the average asylum office
+              grant rate had fallen 36 percent fro an average of 44 percent in
+              fiscal year 2016 to 28 percent in fiscal year 2020
+            </p>
+          </div>
+
+          <div className="bottom-section-text-container">
+            <h1>5%</h1>
+            <p>
+              The New York asylum office grant rate dropped to 5 percent in
+              fiscal year 2020.
+            </p>
+          </div>
+
+          <div className="bottom-section-text-container">
+            <h1>6x Lower</h1>
+            <p>
+              Between fiscal year 2017 and 2020, the New York asylum office’s
+              average grant rate was six times lower than the San Francisco
+              asylum office.
+            </p>
+          </div>
+        </div>
+      </section>
+      <p onClick={() => scrollToTop()} className="back-to-top">
+        Back To Top ^
+      </p>
     </div>
   );
 }
